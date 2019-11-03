3301cb5ce5c37d9ab685dd14364a5ce7409a0bc6
Joseph Lyon <joeylyon1@gmail.com>
2019-11-02 20:28:17 -0400

    vue and bulma

--- api.go
+++ api.go
@@ -31,6 +31,8 @@ func NewServer(url string, port string, uuid uuid.UUID, s *Store) *API {
 	api.s = s
 	api.router.Get("/", api.IndexHandler)
 	api.router.Get("/app.js", api.ScriptHandler)
+	api.router.Get("/vue.js", api.VueHandler)
+	api.router.Get("/bulma.css", api.CSSHandler)
 	api.router.Get("/uuid", api.GetUUID)
 	api.router.Get("/info", api.GetMyInfo)
 	api.router.Get("/messages", api.GetAllMessages)
@@ -47,6 +49,16 @@ func (a *API) IndexHandler(w http.ResponseWriter, r *http.Request) {
 // ScriptHandler ...
 func (a *API) ScriptHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/app.js")
+}
+
+// ScriptHandler ...
+func (a *API) VueHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/vue.js")
+}
+
+// ScriptHandler ...
+func (a *API) CSSHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/bulma.css")
 }
 
 func (a *API) GetAllMessages(w http.ResponseWriter, r *http.Request) {
